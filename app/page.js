7ed64c53b2038e4343dab8d684376a69c4434f4f
alilnil7 <131680888+alilnil7@users.noreply.github.com>
2025-02-07 10:53:51 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import { useCart } from "./context/CartContext";
 
 const products = [
   { id: 1, name: "apple", price: 50, image: "/Apple.png" },
-  { id: 2, name: "Banana", price: 70, image: "/banana.png" },
+  { id: 2, name: "banana", price: 70, image: "/banana.png" },
   { id: 3, name: "grape", price: 120, image: "/grape.png" },
 ];
 
@@ -21,12 +21,14 @@ export default function Home() {
             key={product.id}
             className="flex flex-col items-center border p-4 rounded-lg shadow-lg"
           >
+            {/** Replace Image with img for debugging if needed */}
             <Image
               src={product.image}
               alt={product.name}
               width={150}
               height={150}
               className="rounded-lg"
+              priority={true} // Optional for optimization
             />
             <h2 className="text-lg font-bold">{product.name}</h2>
             <p className="text-sm text-gray-500">{product.price} ₽</p>
@@ -34,7 +36,7 @@ export default function Home() {
               onClick={() => addToCart(product)}
               className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
             >
-              add to cart
+              Add to Cart
             </button>
           </div>
         ))}
